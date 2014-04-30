278075def45030e024958b740d03ac2548d71547
--global <raynos2@gmail.com>
2014-04-29 23:35:26 -0700

    added sublime Preferences

--- sync-sublime.sh
+++ sync-sublime.sh
@@ -1,3 +1,8 @@
+SRC="$HOME/.config/sublime-text-3"
+DST="./.config/sublime-text-3"
 
-cp ~/.config/sublime-text-3/sublime-projects/*.sublime-project \
-	./.config/sublime-text-3/sublime-projects
+cp $SRC/sublime-projects/*.sublime-project \
+    $DST/sublime-projects
+
+cp $SRC/Packages/User/Preferences.sublime-settings \
+    $DST/Packages/User/Preferences.sublime-settings
