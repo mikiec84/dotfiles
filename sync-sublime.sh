3654b0c37a45cabba200a0c9a323c67415941ed3
Raynos <raynos2@gmail.com>
2015-01-18 17:09:51 -0800

    sublime projects should not be here...

--- sync-sublime.sh
+++ sync-sublime.sh
@@ -1,8 +1,8 @@
 SRC="$HOME/.config/sublime-text-3"
 DST="./.config/sublime-text-3"
 
-cp $SRC/sublime-projects/*.sublime-project \
-    $DST/sublime-projects
+# cp $SRC/sublime-projects/*.sublime-project \
+#    $DST/sublime-projects
 
 cp $SRC/Packages/User/Preferences.sublime-settings \
     $DST/Packages/User/Preferences.sublime-settings
